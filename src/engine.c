82d91eb589e0ae5135b98c543592357a9fb75e55
Rakha <rakha.adinindya@gmail.com>
2026-03-03 15:38:35 +0700

    Display all pieces

--- src/engine.c
+++ src/engine.c
@@ -7,6 +7,7 @@ bool init_board(Piece *board[8][8]){
 		}
 	}
 
+	Type pieces[] = {ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK};
 	for(int col='a'; col<='h'; col++){
 		board[col-'a'][2-1] = malloc(sizeof(Piece));
 		if(!board[col-'a'][2-1]) return false;
@@ -15,7 +16,16 @@ bool init_board(Piece *board[8][8]){
 		board[col-'a'][7-1] = malloc(sizeof(Piece));
 		if(!board[col-'a'][7-1]) return false;
 		*board[col-'a'][7-1] = (Piece){BLACK, PAWN};
+
+		board[col-'a'][1-1] = malloc(sizeof(Piece));
+		if(!board[col-'a'][1-1]) return false;
+		*board[col-'a'][1-1] = (Piece){WHITE, pieces[col-'a']};
+
+		board[col-'a'][8-1] = malloc(sizeof(Piece));
+		if(!board[col-'a'][8-1]) return false;
+		*board[col-'a'][8-1] = (Piece){BLACK, pieces[col-'a']};
 	}
+
 
 	return true;
 }
